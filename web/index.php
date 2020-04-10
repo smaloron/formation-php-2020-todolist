49fdc22b8848cf46df1f81f6bf33a7a2c54f94d7
Sebastien Maloron <contact@smaloron.eu>
2020-04-10 11:52:34 +0200

    Création de la connexion à la BD avec une instance de PDO

--- web/index.php
+++ web/index.php
@@ -12,5 +12,16 @@ if(! file_exists($controllerPath)){
     $controllerPath = "../controllers/errorController.php";
 }
 
+//Ouverture de le connexion à la base de données
+$pdo = new PDO(
+    "mysql:host=127.0.0.1;dbname=todolist;charset=utf8",
+    "root",
+    "",
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
+
 //Exécution du contrôleur
 require $controllerPath;
