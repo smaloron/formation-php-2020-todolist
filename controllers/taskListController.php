220205247825c340c07a2b0f2b79163e388f1600
Sebastien Maloron <contact@smaloron.eu>
2020-04-10 12:09:22 +0200

    Reuête et affichage de la liste des tâche

--- controllers/taskListController.php
+++ controllers/taskListController.php
@@ -1,5 +1,9 @@
 <?php
 
+//Requête sur la BD pour afficher la liste des tâches
+$recordSet = $pdo->query("SELECT * FROM view_tasks");
+$taskList = $recordSet->fetchAll();
+
 $pageTitle = "liste des tâches";
 $content = "taskList";
 
--- views/taskList.php
+++ views/taskList.php
@@ -1,1 +1,27 @@
 <h1><?= $pageTitle ?></h1>
+
+<table>
+
+    <tr>
+        <th>Titre</th>
+        <th>Catégorie</th>
+        <th>Statuts</th>
+        <th>Avancement</th>
+        <th>Date d'échéance</th>
+    </tr>
+
+    <?php foreach ($taskList as $task): ?>
+        <?php 
+            //Transforme les lef d'un tableau associatif en une suite de variables
+            extract($task)
+        ?>
+        <tr>
+            <td><?= $title ?></td>
+            <td><?= $category_name ?></td>
+            <td><?= $status_name ?></td>
+            <td><?= $completion_rate ?></td>
+            <td><?= $due_date ?></td>
+        </tr>
+    <?php endforeach ?>
+
+</table>
